65f9a91f612f3bd7d1e0f35144b1a6bf0ec3f4fd
oXygen XML Deployer <support@oxygenxml.com>
2019-09-26 15:08:03 +0000

    rebuild pages at 91040a9

---
+++ master/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,43 @@
+define(function() {var keywords=[{w:"Frequently",p:["p0"]},{w:"Asked",p:["p0"]},{w:"Questions",p:["p0"]},{w:"Thunderbird",p:["p1"]},{w:"StormCluster",p:["p1","p25"]},{w:"features",p:["p1"]},{w:"and",p:["p1"]},{w:"benefits",p:["p1"]},{w:"cluster",p:["p2","p4","p33","p35"]},{w:"capacity",p:["p3","p4"]},{w:"reports",p:["p4"]},{w:"About",p:["p5","p8"]},{w:"this",p:["p5"]},{w:"guide",p:["p5"]},{w:"MobileView",p:["p6","p8","p15","p31"]},{w:"general",p:["p7"]},{w:"introduction",p:["p7"]},{w:"customize",p:["p9","p10"]},{w:"views",p:["p9","p10","p23","p24","p29"]},{w:"tab",p:["p9","p11","p16","p18"]},{w:"system",p:["p11","p12","p16","p17","p26","p27","p28"]},{w:"diagnostics",p:["p11","p12"]},{w:"messaging",p:["p13"]},{w:"overview",p:["p13"]},{w:"organization",p:["p14"]},{w:"How",p:["p15"]},{w:"is",p:["p15"]},{w:"organized",p:["p15"]},{w:"performance",p:["p16","p17"]},{w:"workspace",p:["p18","p19"]},{w:"environment",p:["p19"]},{w:"ClusterView",p:["p20"]},{w:"query",p:["p21","p22"]},{w:"filters",p:["p21"]},{w:"warning",p:["p22"]},{w:"messages",p:["p22"]},{w:"data",p:["p23","p24","p29"]},{w:"Quick",p:["p24","p27"]},{w:"reference:",p:["p24","p27"]},{w:"health",p:["p26","p27"]},{w:"indicators",p:["p26","p27"]},{w:"notifications",p:["p28"]},{w:"generating",p:["p29"]},{w:"logging",p:["p30","p31"]},{w:"in",p:["p30"]},{w:"on",p:["p31"]},{w:"to",p:["p31"]},{w:"ClusterControl",p:["p32"]},{w:"reporting",p:["p33","p35"]},{w:"troubleshooting",p:["p34","p35"]},{w:"problems",p:["p35"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6, 7];
+ph["p2"]=[8];
+ph["p3"]=[9];
+ph["p4"]=[8, 9, 10];
+ph["p5"]=[11, 12, 13];
+ph["p6"]=[14];
+ph["p7"]=[15, 16];
+ph["p8"]=[11, 14];
+ph["p9"]=[17, 18, 19];
+ph["p30"]=[43, 44];
+ph["p10"]=[17, 18];
+ph["p32"]=[47];
+ph["p31"]=[43, 45, 46, 14];
+ph["p12"]=[20, 21];
+ph["p34"]=[49];
+ph["p11"]=[20, 21, 19];
+ph["p33"]=[8, 48];
+ph["p14"]=[24];
+ph["p13"]=[22, 23];
+ph["p35"]=[49, 8, 48, 50];
+ph["p16"]=[20, 28, 19];
+ph["p15"]=[25, 14, 26, 27];
+ph["p18"]=[29, 19];
+ph["p17"]=[20, 28];
+ph["p19"]=[29, 30];
+ph["p21"]=[32, 33];
+ph["p20"]=[31];
+ph["p23"]=[36, 18];
+ph["p22"]=[32, 34, 35];
+ph["p25"]=[4];
+ph["p24"]=[37, 38, 36, 18];
+ph["p27"]=[37, 38, 20, 39, 40];
+ph["p26"]=[20, 39, 40];
+ph["p29"]=[42, 36, 18];
+ph["p28"]=[20, 41];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
